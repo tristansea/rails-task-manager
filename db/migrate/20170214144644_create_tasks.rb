bd141bd9f7946f649bd651eb0df20ecc7283c35b
Tristan Cummins <surfrt16@gmail.com>
2017-02-14 17:20:05 +0000

    yup

--- db/migrate/20170214144644_create_tasks.rb
+++ db/migrate/20170214144644_create_tasks.rb
@@ -1,8 +1,8 @@
 class CreateTasks < ActiveRecord::Migration[5.0]
   def change
     create_table :tasks do |t|
-      t.string :name
-      t.boolean :completed
+      t.string :title
+      t.boolean :completed, :default => false
 
       t.timestamps
     end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170214120622) do
+ActiveRecord::Schema.define(version: 20170214144644) do
 
   create_table "tasks", force: :cascade do |t|
-    t.string   "name"
-    t.boolean  "completed"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.string   "title"
+    t.boolean  "completed",  default: false
+    t.datetime "created_at",                 null: false
+    t.datetime "updated_at",                 null: false
   end
 
 end
